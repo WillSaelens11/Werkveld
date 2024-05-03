be47c41a0e73d27156cead809a14857bdf5f3aa4
WillSaelens11 <67610361+WillSaelens11@users.noreply.github.com>
2024-05-03 09:44:26 +0200

    Github repository aangemaakt en gelinked met jira

---
+++ solution1/Werkveld/Tabel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Werkveld
+{
+    public class Tabel
+    {
+        public TypeTabel TypeTabel { get; set; }
+        public List<Kolom> Kolommen { get; set; }          // Of maak er een hashset van. 
+        public List<List<object>> Rijen { get; set; }
+
+        public void VoegkolomToe(Kolom kolom)
+        {
+            if (Kolommen.Any(c => c.Naam == kolom.Naam))
+            {
+                throw new DomeinException("Een kolom met dezelfde naam bestaat al in de tabel.");
+            }
+
+            Kolommen.Add(kolom);
+        }
+    }
+}
---
+++ solution1/Werkveld/TypeTabel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Werkveld
+{
+    public class TypeTabel
+    {
+        public int TypeID { get; set; }
+        public string Naam { get; set; }
+        public EnumTypeTabel TypeTabellen { get; set; }
+        public int? AantalKolommen { get; set; } 
+        public List<TypeKolom> MogelijkeKolommen { get; set; }
+        public int? MinKolommen{ get; set; }
+    }
+}

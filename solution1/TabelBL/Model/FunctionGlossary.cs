ec2c50c62f64043c4e1f3b4ce325ba1bc0b7ce84
kennethdobbelaere <147134845+kennethdobbelaere@users.noreply.github.com.>
2024-05-20 11:50:41 +0200

    model

---
+++ solution1/TabelBL/Model/FunctionGlossary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabelBL.Inferfaces;
+
+namespace TabelBL.Model
+{
+    public class FunctionGlossary
+    {
+        public string Naam { get; set; }
+        public string DataType { get; set; }
+        public int AantalKolommen { get; set; }
+        public int AantalRijen { get; set; }
+    }
+}

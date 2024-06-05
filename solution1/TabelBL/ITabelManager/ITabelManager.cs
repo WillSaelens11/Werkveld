64b25df8274a453328fc4213c4cf3fba47c47152
WillSaelens11 <67610361+WillSaelens11@users.noreply.github.com>
2024-06-05 19:06:24 +0200

    Geprobeerd een wpf toe te voegen.

---
+++ solution1/TabelBL/ITabelManager/ITabelManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabelDL_SQL;
+using Werkveld;
+
+namespace TabelBL.ITabelManager
+{
+    /*public interface ITabelManager
+    {
+        List<Tabel> Tabellen { get; }
+        void VoegTabelToe(Tabel tabel);
+        void VerwijderTabel(Tabel tabel);
+        void VoegKolomToe(Tabel tabel, Kolom kolom);
+        void VerwijderKolom(Tabel tabel, Kolom kolom);
+        void VoegRijToe(Tabel tabel);
+        void VerwijderRij(Tabel tabel, Rij rij);
+        bool BestaatRij(Tabel tabel, Rij rij);
+        HashSet<Kolom> AlleKolommen();
+        bool BestaatTabel(Tabel tabel);
+        bool BestaatKolom(Tabel tabel, Kolom kolom);
+    }*/
+
+}
---
+++ solution1/WPF1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DataTable dt = new DataTable();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dt.Columns.Add("Kolom 1", typeof(string));
+            dt.Columns.Add("Kolom 2", typeof(string));
+            dt.Columns.Add("Kolom 3", typeof(string));
+
+            dataGrid.ItemsSource = dt.DefaultView;
+        }
+
+        private void VoegRijToe_Click(object sender, RoutedEventArgs e)
+        {
+            dt.Rows.Add("", "", "");
+        }
+
+        private void VerwijderRij_Click(object sender, RoutedEventArgs e)
+        {
+            int index = dataGrid.SelectedIndex;
+            if (index >= 0 && index < dt.Rows.Count)
+            {
+                dt.Rows.RemoveAt(index);
+            }
+        }
+
+        private void VoegKolomToe_Click(object sender, RoutedEventArgs e)
+        {
+            dt.Columns.Add("Nieuwe Kolom", typeof(string));
+        }
+
+        private void VerwijderKolom_Click(object sender, RoutedEventArgs e)
+        {
+            if (dt.Columns.Count > 0)
+            {
+                dt.Columns.RemoveAt(dt.Columns.Count - 1);
+            }
+        }
+    }
+}

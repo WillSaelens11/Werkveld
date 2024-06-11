066bce34bae10412a5d2ff28aeff17d14a4ce93d
kennethdobbelaere <147134845+kennethdobbelaere@users.noreply.github.com.>
2024-06-11 09:52:46 +0200

    bb

---
+++ Solution1/Werkveld/Repo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabelBL.Interfaces;
+
+namespace TabelDL_SQL
+{
+    public class Repo : IRepo
+    {
+        private string _connectionString;
+
+        public Repo(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+    }
+
+}

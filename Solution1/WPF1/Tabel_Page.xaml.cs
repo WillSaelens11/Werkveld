765df89cef7cda8c9c65b9a745f9f867d78d3fb5
WillSaelens11 <67610361+WillSaelens11@users.noreply.github.com>
2024-06-12 19:22:38 +0200

    Extra kolommen, Wijzig kolomnaam functionaliteit toegevoegd en button toegevoegd.

--- Solution1/WPF1/Tabel_Page.xaml.cs
+++ Solution1/WPF1/Tabel_Page.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -26,6 +27,11 @@ namespace WPF1
     public partial class Tabel_Page : Page
     {
         private DataTable dt = new DataTable();
+
+
+        private static Dictionary<string, int> instanceCounts = new Dictionary<string, int>();
+
+
         public Tabel_Page(Type type)
         {
             InitializeComponent();
@@ -39,16 +45,26 @@ namespace WPF1
             //    }
             //}
 
+            string typeName = type.Name;
+            if (!instanceCounts.ContainsKey(typeName))
+            {
+                instanceCounts[typeName] = 0;
+            }
+            instanceCounts[typeName]++;
+
+            // Stel de titel van de pagina in op basis van het type tabel en het aantal instanties
+            this.Title = "Tabel " + typeName + " " + instanceCounts[typeName];
+
             dt.Columns.Add("Kolom 1", typeof(string));
             dt.Columns.Add("Kolom 2", typeof(string));
-            dt.Columns.Add("Kolom 3", typeof(string));
+            /*dt.Columns.Add("Kolom 3", typeof(string));*/
 
             dataGrid.ItemsSource = dt.DefaultView;
         }
 
         private void VoegRijToe_Click(object sender, RoutedEventArgs e)
         {
-            dt.Rows.Add("", "", "");
+            dt.Rows.Add("", "");
         }
 
         private void VerwijderRij_Click(object sender, RoutedEventArgs e)
@@ -72,5 +88,37 @@ namespace WPF1
                 dt.Columns.RemoveAt(dt.Columns.Count - 1);
             }
         }
+
+
+        // 
+
+        public void WijzigKolomNaam(int kolomIndex, string nieuweNaam)
+        {
+            if (kolomIndex >= 0 && kolomIndex < dataGrid.Columns.Count)
+            {
+                dataGrid.Columns[kolomIndex].Header = nieuweNaam;
+            }
+
+        }
+
+
+        private void DataGridColumnHeader_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            DataGridColumnHeader column = sender as DataGridColumnHeader;
+            if (column != null)
+            {
+                string nieuweNaam = Microsoft.VisualBasic.Interaction.InputBox("Voer de nieuwe kolomnaam in", "Wijzig kolomnaam", column.Content.ToString());
+                if (!string.IsNullOrEmpty(nieuweNaam))
+                {
+                    column.Content = nieuweNaam;
+                }
+            }
+        }
+
+        private void SaveData_Click(object sender, RoutedEventArgs e)
+        {
+            // Implementeer hier je logica voor het opslaan van de data
+        }
     }
+
 }
